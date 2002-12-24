8a81f1799d6a96b9b471df4806384728ab2dc37e
rbj <rbj>
2002-12-24 16:02:49 +0000

    Imported Sources

---
+++ src/common/rules.mk
@@ -0,0 +1,167 @@
+# $Id: rules.mk,v 1.1 2002/12/24 16:02:49 rbj Exp $
+
+.SUFFIXES:
+.SUFFIXES: .css .doc .gif .html .in .sml .xml .xdoc .xsl
+.PHONY: build $(SUBBUILDS) vars thisinstall $(THISINSTALL) install \
+	uninstall $(UNINSTALL) $(SUBINSTALLS)
+
+vpath *.in $(SRCDIR)
+vpath *.xsl $(COMDIR)
+
+Makefile: $(MKDEPS)
+	cd $(TOPSRCDIR); ./configure --prefix=$(prefix)
+
+# Qualified rules
+
+XMLT001=$(HTML001:.html=.xmlt)
+
+$(XMLT001): %.xmlt: %.xml
+	cp $<  $*.xmlt
+
+$(HTML003): %.html: %.xml xslt003$(XSLTSUFF).xsl xslt002.xsl X-Logic.xsl
+	$(JAVA) $(XSLTPROC) $< $(XLCOMDIR)/xslt003$(XSLTSUFF).xsl
+
+XMLT004=$(HTML004:.html=.xmlt)
+
+$(XMLT004): %.xmlt: %.xml
+	addftl <$<  >$*.xmlx
+	xxml2xml <$*.xmlx >$*.xmlt
+
+$(HTML001): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl X-Logic.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+
+$(HTML004): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+
+$(HTML004i) $(HTML001i): %-i.html : %.html
+
+$(HTML004m) $(HTML001m): %-m.html : %.html
+
+HTMLTHLS=$(PPTHD:.thd=.html)
+
+$(HTMLTHLS): %.html: %.xml xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
+	xxml2xml <$*.xml >$*.xmlt
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+
+$(DPDIRCPY): %: $(DPDIR)/%
+	cp $(DPDIR)/$@ .
+
+$(SRCDIRCPY): %: $(SRCDIR)/%
+	cp $(SRCDIR)/$@ .
+
+$(XLCDIRCPY): %: $(XLCOMDIR)/%
+	cp $(XLCOMDIR)/$@ .
+
+$(XLDPDIRCPY): %: $(XLDPDIR)/%
+	cp $(XLDPDIR)/$@ .
+
+$(SUBINSTALLS): %.install: 
+	cd $* && $(MAKE) install
+
+$(SUBBUILDS): %.build: 
+	cd $* && $(MAKE) build
+
+$(PPDOC): %.doc : %.xml
+	xml2ppdoc <$^ >$*.doc
+
+$(PPDOCSML): %.sml: %.doc
+	docsml -f $(DOCPREPDATA)/sieveview $*
+
+$(PPDOCXDOC): %.xdoc:
+	ppdoc2xml <$*.doc >$(SRCDIR)/$*.xml
+
+%.xmldoc: %.doc
+	sieve -f $(DOCPREPDATA)/sieveview xmldoc <$*.doc >$*.xmldoc
+
+PPTHDXLD=$(PPTHD:.thd=.xml)
+
+$(PPTHDXLD): %.xml: %.thd
+	ppthd2xml $* $(WEBROOTDIR)
+
+$(PPDB).dbts:
+	if test "$(PPDBDIR)" != ""; then ln -s $(PPDBDIR)$(PPDB).$(PPDBARCH); fi
+	cd ./$(PPDBDIR); if test ! -f $(PPDB).$(PPDBARCH); then pp_make_database -p hol $(PPDB); fi
+	touch $(PPDB).dbts
+
+$(PPLDS): %.lds: %.sml $(PPDB).dbts
+	echo "save_and_quit();" | pp -d $(PPDB) -f $< > $*.log
+	touch $@
+
+$(PPTHD): %.thd: %.th
+	pp_list -d $(PPDB) $* > $*.thd
+
+$(BASHBIN): % : %.sh
+	cp $< $*
+	chmod +x $*
+
+$(PERLBIN): % : %.pl
+	cp $< $*
+	chmod +x $*
+
+# Unqualified rules
+
+
+# specific rules
+
+installbins:
+	@if ! test -d $(BINDIR) &&  test "" != "$(BINFILES)"; then $(INSTALL) -d -m 755 $(BINDIR); fi
+	if test "" != "$(BINFILES)"; then $(INSTALL) -m 755 $(BINFILES) $(BINDIR); fi
+
+installdata:
+	@if ! test -d $(RDATADIR) && test "" != "$(DATAFILES)"; then $(INSTALL) -d -m 755 $(RDATADIR); fi
+	if test "" != "$(DATAFILES)"; then $(INSTALL) -m 644 $(DATAFILES) $(RDATADIR); fi
+
+installlibs:
+	@if ! test -d $(LIBDIR) &&  test "" != "$(LIBFILES)"; then $(INSTALL) -d -m 755 $(LIBDIR); fi
+	if test "" != "$(LIBFILES)"; then $(INSTALL) -m 644 $(LIBFILES) $(LIBDIR); fi	
+
+installperllibs:
+	@if ! test -d $(PERLLIBDIR) &&  test "" != "$(PERLLIBFILES)"; then $(INSTALL) -d -m 755 $(PERLLIBDIR); fi
+	if test "" != "$(PERLLIBFILES)"; then $(INSTALL) -m 644 $(PERLLIBFILES) $(PERLLIBDIR); fi	
+
+installsmllibs:
+	@if ! test -d $(SMLLIBDIR) &&  test "" != "$(SMLLIBFILES)"; \
+	then $(INSTALL) -d -m 755 $(SMLLIBDIR); fi
+	if test "" != "$(SMLLIBFILES)"; \
+	then $(INSTALL) -m 644 $(SMLLIBFILES) $(SMLLIBDIR); cp -r CM $(SMLLIBDIR); fi
+
+installweb: $(WEBFILES)
+	@if ! test -d $(RWEBDIR) &&  test "" != "$(WEBFILES)"; then $(INSTALL) -d -m 755 $(RWEBDIR); fi
+	if test "" != "$(WEBFILES)"; then $(INSTALL) -m 644 $(WEBFILES) $(RWEBDIR); fi
+
+THISINSTALL=installweb installdata installbins installlibs installperllibs installsmllibs
+
+ppthds: $(PPTHD)
+
+build: $(BUILDFIRST) $(SUBBUILDS) $(BINFILES) $(DATAFILES) $(LIBFILES) $(PERLLIBFILES) \
+	$(SMLLIBFILES) $(WEBFILES)
+
+install: build $(THISINSTALL) $(SUBINSTALLS)
+
+# from x-logic xldoc.mk
+
+# Variable definitions
+
+
+# Qualified rules
+
+$(XMLT005): %.xmlt: %.xml
+	addftl <$<  | xxml2xml >$*.xmlt
+
+$(HTML005f): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl IndexFrame.xsl MainFrame.xsl pp-symbol.ent ppft.xsl X-Logic.xsl sitespecific.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+
+$(XFTFILES): %.xft: %.xml stripfile.xsl
+	$(JAVA) com.jclark.xsl.sax.Driver $< $(XLCOMDIR)/stripfile.xsl
+	touch $*.xft
+
+$(HTML005i): %-i.html : %.html
+
+$(HTML005m): %-m.html : %.html
+
+$(XMLT006): %.xmlt: %.xml
+	addftl <$<  | xxml2xml >$*.xmlt
+
+$(HTML006): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl IndexFrame.xsl MainFrame.xsl pp-symbol.ent ppft.xsl X-Logic.xsl sitespecific.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+
---
+++ src/common/sitespecific.xsl
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+
+<!-- stylesheet for site specific templates 
+     $Id: sitespecific.xsl,v 1.1 2002/12/24 16:02:49 rbj Exp $ -->
+
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+				version="1.0"
+				xmlns="http://www.w3.org/TR/xhtml1/strict"
+				xmlns:xld="http://www.x-logic.org/xmlns/draft/xld"
+				xsl:exclude-result-prefixes="xld">
+
+<xsl:template name="domain">
+  <xsl:text>www.rbjones.com</xsl:text>
+</xsl:template>
+
+</xsl:stylesheet>
+
+
+
