6250de19a724cde66148f7bcb3f57cf72b876137
rbj01 <rbj01>
2006-03-25 22:50:37 +0000

    Update to restore cvs after period of working without it.

--- src/common/rules.mk
+++ src/common/rules.mk
@@ -1,12 +1,13 @@
-# $Id: rules.mk,v 1.6 2005/04/09 14:00:53 rbj Exp $
+# $Id: rules.mk,v 1.7 2006/03/25 22:50:36 rbj01 Exp $
 
 .SUFFIXES:
 .SUFFIXES: .css .doc .gif .html .in .sml .xml .xdoc .xsl
 .PHONY: build $(SUBBUILDS) vars thisinstall $(THISINSTALL) install \
 	uninstall $(UNINSTALL) $(SUBINSTALLS)
 
-vpath *.in $(SRCDIR)
-vpath *.xsl $(COMDIR)
+vpath %.in $(SRCDIR)
+vpath %.xsl $(COMDIR)
+vpath %.sxx $(XLDPDIR)
 
 Makefile: $(MKDEPS)
 	cd $(TOPSRCDIR); ./configure --prefix=$(prefix)
@@ -14,7 +15,8 @@ Makefile: $(MKDEPS)
 # variable definitions
 # the following variables should be defined in the makefile if required
 # PPTHTO = (.th) names of proofpower theories for listing in TEX only
-# PPTH	= (.th) names of proofpower theories for listing in HTML
+# PPTH	= (.th) names of proofpower application theories for listing in HTML
+# PPPPTH	= (.th) names of proofpower built-in theories for listing in HTML
 # HTML001	= (.html) HTML documents
 # HTML004	= (.html) HTML documents
 
@@ -22,13 +24,19 @@ Makefile: $(MKDEPS)
 
 # HTML from XML
 
-XMLT001=$(HTML001:.html=.xmlt)
-XMLT004=$(HTML004:.html=.xmlt)
+XMLT001=$(HTML001:.html=.xmlt) $(HTM001:.htm=.xmlt)
+XMLT004=$(HTML004:.html=.xmlt) $(HTM004:.htm=.xmlt)
 
 # Theory listings
+# first for ProofPower built-in theories
+
+PPPPTHD=$(PPPPTH:.th=.thd)
+PPPPTHDOC=$(PPPPTH:.th=.th.doc)
+
+# then for application theories
 
 PPTHD=$(PPTH:.th=.thd)
-PPTHDOC=$(PPTH:.th=.th.doc)
+PPTHDOC=$(PPTH:.th=.th.doc) $(PPPPTHDOC)
 PPTHTEX=$(PPTHDOC:.doc=.tex)
 HTMLTHLS=$(PPTHDOC:.th.doc=.html)
 PPTHDXLD=$(PPTHDOC:.th.doc=.xml)
@@ -50,7 +58,7 @@ PDF=$(PS:.ps=.pdf)
 
 # Qualified dependencies
 
-$(KLUDVI): rbjk.bib
+$(KLUDVI): rbjk.bib rbjk.cls
 
 # Qualified rules
 
@@ -58,25 +66,34 @@ $(XMLT001): %.xmlt: %.xml
 	cp $<  $*.xmlt
 
 $(HTML003): %.html: %.xml xslt003$(XSLTSUFF).xsl xslt002.xsl X-Logic.xsl
-	$(JAVA) $(XSLTPROC) $< $(XLCOMDIR)/xslt003$(XSLTSUFF).xsl
+	$(JAVA) $(XSLTPROC) $< $(XLCOMDIR)/xslt003$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
 
 $(XMLT004): %.xmlt: %.xml
 	addftl <$<  >$*.xmlx
 	xxml2xml <$*.xmlx >$*.xmlt
 
 $(HTML001): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl X-Logic.xsl
-	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
+
+$(HTM001): %.htm: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl X-Logic.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
+
+$(HTM004): %.htm: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
 
 $(HTML004): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
-	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
 
 $(HTML004i) $(HTML001i): %-i.html : %.html
 
 $(HTML004m) $(HTML001m): %-m.html : %.html
 
-$(HTMLTHLS): %.html: %.xml xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
+$(HTMLTHLS): %.html: %.xml $(XLDPDIR)/xslt001s8.sxx xslt002.xsl frame01.xsl pp-symbol.ent ppft.xsl X-Logic.xsl
 	xxml2xml <$*.xml >$*.xmlt
-	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+	$(JAVA) $(XSLT2PROC) -c $*.xmlt $(XLDPDIR)/xslt001s8.sxx root=$(BLDROOT) dir=$(RELWEBDIR)/ name=$*
+
+$(COMDIRCPY): %: $(COMDIR)/%
+	cp $(COMDIR)/$@ .
 
 $(DPDIRCPY): %: $(DPDIR)/%
 	cp $(DPDIR)/$@ .
@@ -108,7 +125,7 @@ $(PPDOCXDOC): %.xdoc:
 %.xmldoc: %.doc
 	sieve -f $(DOCPREPDATA)/sieveview xmldoc <$*.doc >$*.xmldoc
 
-$(PPTHDXLD): %.xml: %.thd
+$(PPTHDXLD) $(PPPPTHDXLD): %.xml: %.thd
 	ppthd2xml $* $(WEBROOTDIR)
 
 $(PPDB).dbts:
@@ -119,6 +136,9 @@ $(PPDB).dbts:
 $(PPLDS): %.lds: %.sml $(PPDB).dbts
 	echo "save_and_quit();" | pp -d $(PPDB) -f $< > $*.log
 	touch $@
+
+$(PPPPTHD): %.thd: %.th
+	pp_list -d $(PPBASEDB) $* > $*.thd
 
 $(PPTHD) $(PPTHTOD): %.thd: %.th
 	pp_list -d $(PPDB) $* > $*.thd
@@ -186,7 +206,7 @@ installweb: $(WEBFILES)
 
 THISINSTALL=installweb installdata installbins installlibs installperllibs installsmllibs
 
-ppthds: $(PPTHD)
+ppthds: $(PPTHD) $(PPPPTHD)
 
 build: $(BUILDFIRST) $(SUBBUILDS) $(BINFILES) $(DATAFILES) $(LIBFILES) $(PERLLIBFILES) \
 	$(SMLLIBFILES) $(WEBFILES) $(BUILDEXTRAS)
@@ -197,26 +217,41 @@ install: build $(THISINSTALL) $(SUBINSTALLS)
 
 # Variable definitions
 
-
 # Qualified rules
 
-$(XMLT005): %.xmlt: %.xml
+$(XMLT005S) $(XMLT005): %.xmlt: %.xml
 	addftl <$<  | xxml2xml >$*.xmlt
 
 $(HTML005f): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl IndexFrame.xsl MainFrame.xsl pp-symbol.ent ppft.xsl X-Logic.xsl sitespecific.xsl
-	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
+
+#$(HTM005f): %.htm: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl IndexFrame.xsl MainFrame.xsl pp-symbol.ent ppft.xsl X-Logic.xsl sitespecific.xsl
+#	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
+
+$(HTM005f): %.htm: %.xmlt $(XLDPDIR)/xslt001s8.sxx pp-symbol.ent
+	$(JAVA) $(XSLT2PROC) -c $*.xmlt $(XLDPDIR)/xslt001s8.sxx root=$(BLDROOT) \
+		dir=$(RELWEBDIR)/ name=$* suffix=htm imagedir=rbjgifs 
 
 $(XFTFILES): %.xft: %.xml stripfile.xsl
-	$(JAVA) com.jclark.xsl.sax.Driver $< $(XLCOMDIR)/stripfile.xsl
+	$(JAVA) $(XSLTPROC) $< $(XLCOMDIR)/stripfile.xsl dir=$(BLDDIR) name=$*
 	touch $*.xft
 
 $(HTML005i) $(HTMLTHLSi): %-i.html : %.html
 
 $(HTML005m) $(HTMLTHLSm): %-m.html : %.html
 
-$(XMLT006): %.xmlt: %.xml
+$(HTM005i) $(HTMTHLSi): %-i.htm : %.htm
+
+$(HTM005m) $(HTMTHLSm): %-m.htm : %.htm
+
+$(XMLT006) $(XMLT007): %.xmlt: %.xml
 	addftl <$<  | xxml2xml >$*.xmlt
 
 $(HTML006): %.html: %.xmlt xslt001$(XSLTSUFF).xsl xslt002.xsl frame01.xsl IndexFrame.xsl MainFrame.xsl pp-symbol.ent ppft.xsl X-Logic.xsl sitespecific.xsl
-	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl
+	$(JAVA) $(XSLTPROC) $*.xmlt $(XLCOMDIR)/xslt001$(XSLTSUFF).xsl dir=$(BLDDIR) name=$*
 
+$(HTML007f): %.html: %.xmlt $(XLDPDIR)/xslt001s8.sxx pp-symbol.ent
+	$(JAVA) $(XSLT2PROC) -c $*.xmlt $(XLDPDIR)/xslt001s8.sxx root=$(BLDROOT) \
+		dir=$(RELWEBDIR)/ name=$* imagedir=rbjgifs 
+
+$(HTML007i) $(HTML007m): $(HTML007f)
--- src/common/sitespecific.xsl
+++ src/common/sitespecific.xsl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="iso-8859-1"?>
 
 <!-- stylesheet for site specific templates 
-     $Id: sitespecific.xsl,v 1.1 2002/12/24 16:02:49 rbj Exp $ -->
+     $Id: sitespecific.xsl,v 1.2 2006/03/25 22:50:36 rbj01 Exp $ -->
 
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 				version="1.0"
@@ -11,6 +11,10 @@
 
 <xsl:template name="domain">
   <xsl:text>www.rbjones.com</xsl:text>
+</xsl:template>
+
+<xsl:template name="top">
+  <xsl:text>index.htm</xsl:text>
 </xsl:template>
 
 </xsl:stylesheet>
